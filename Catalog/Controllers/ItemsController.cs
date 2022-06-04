b77c19453229c97afeba1455a2b2b7b0423c5ea9
zabiullla <zabiulla.edu@gmail.com>
2022-06-04 22:04:26 +0530

    Add project files.

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController:ControllerBase
+    {
+        private readonly InMemItemsRepository repositery;
+
+        public ItemsController()
+        {
+            repositery = new InMemItemsRepository();
+        }
+
+        //Get /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repositery.GetItems();
+            return items;
+        }
+
+        //Get /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repositery.GetItem(id);
+            if(item is null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    // use for immutable object
+    // with expression support
+    // value based equility support
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
