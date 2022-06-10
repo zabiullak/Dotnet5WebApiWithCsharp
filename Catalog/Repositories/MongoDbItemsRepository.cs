9b9e548d62c783f7e59607ee2087e9afedafb0b1
zabiulla <zabiulla.edu@gamil.com>
2022-06-10 23:11:03 +0530

    updated MongoDb

---
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemCollection;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public void CreateItem(Item item)
+        {
+            itemCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
